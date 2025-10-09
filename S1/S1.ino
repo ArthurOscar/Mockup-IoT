a087d0a2a84ae1cea747b836a7bb3a80fff87c57
ArthurOscar <arthur_o_soares@estudante.sesisenai.org.br>
2025-10-09 10:02:03 -0300

    Conexão com rede WiFi

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include  <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Conectando no WiFi");
+  WiFi.begin(SSID,PASS); //Tenta conectar na rede
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println(".");
+    delay(200);
+  }
+  Serial.println("\nConectado com Sucesso!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
