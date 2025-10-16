2ace07e9c42f496966d6627ee42913a60f26be67
ArthurOscar <arthur_o_soares@estudante.sesisenai.org.br>
2025-10-16 09:45:17 -0300

    Conectando a Placa no MQTT

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,4 +1,15 @@
-#include  <WiFi.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
+const String BrokerURL = "test.mosquitto.org"; //endereço do broker público
+const int BrokerPort = 1883; //porta do broker público
+
+const String BrokerUser = ""; //usuário do servidor
+const String BrokerPass = ""; //senha do servidor
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
@@ -10,7 +21,18 @@ void setup() {
     Serial.println(".");
     delay(200);
   }
-  Serial.println("\nConectado com Sucesso!");
+  Serial.println("\nConectado com Sucesso no WiFi!");
+
+  Serial.println("Conectando ao Broker...");
+  mqtt.setServer(BrokerURL.c_str(),BrokerPort);
+  String BoardID = "S1";
+  BoardID += String(random(0xffff),HEX);
+  mqtt.connect(BoardID.c_str() , BrokerUser.c_str() , BrokerPass.c_str());
+  while(!mqtt.connected()){
+    Serial.print(".");
+    delay(200);
+  }  
+  Serial.println("\nConectado ao Broker!");
 }
 
 void loop() {
