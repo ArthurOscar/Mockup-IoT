a7820b5a222af60c38c7cb6d07b3a36fba44c43f
lucas <lucas_f_ramos@estudante.sesisenai.org.br>
2025-10-16 09:46:15 -0300

    Conectando a Placa no MQTT

--- s2/s2.ino
+++ s2/s2.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wifi.h>
+#include <PubSubClient.h>
 
+const String BrokerURL = "test.mosquitto.org";
+const int  BrokerPort =1883;
+const String BrokerUser ="";
+const String BrokerPass ="";
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Conectanto ao WiFi"); 
+  WiFi.begin(SSID,PASS);
+  while(WiFi.status() !=WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+}
+Serial.println("\nConectando com Sucesso!");
+
+Serial.println("Conectando ao Broker...");
+mqtt.setServer(BrokerURL.c_str(),BrokerPort);
+String BoardID = "S2";
+BoardID += String(random(0xffff),HEX);
+mqtt.connect(BoardID.c_str(), BrokerUser.c_str() , BrokerPass.c_str());
+
+While(!mqtt.connected()){
+  Serial.print(".");
+  delay(200);
+ }
+ Serial,println("\nConectado ao Broker")
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
